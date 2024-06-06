c471879fe006dcd01a872c413f29f0b4e0c14e15
giuseppebarbetta <giuseppebarbetta@outlook.com>
2024-06-05 21:17:20 -0300

    fix: format code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 
 import { connectDatabase } from "./database/db.js";
 import routes from "./routes.js";
@@ -8,11 +8,13 @@ const app = express();
 const port = 3003;
 
 app.use(express.json());
-app.use(cors())
-app.use(routes)
+app.use(cors());
+app.use(routes);
 
 connectDatabase()
   .then(() => {
-    app.listen(port, () => console.log("🚀 Servidor e banco de dados rodando na porta: ", port));
+    app.listen(port, () =>
+      console.log("🚀 Servidor e banco de dados rodando na porta: ", port)
+    );
   })
   .catch((err) => console.log(err));
--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,29 @@
-import { Router } from 'express'
-import { createMovie, deleteMovie, getMovies, getMoviesAndCategories, updateMovie } from './controllers/MovieController.js'
-import { createCategory, deleteCategory, getCategories, updateCategory } from './controllers/CategoryController.js'
+import { Router } from "express";
+import {
+  createMovie,
+  deleteMovie,
+  getMovies,
+  getMoviesAndCategories,
+  updateMovie,
+} from "./controllers/MovieController.js";
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  updateCategory,
+} from "./controllers/CategoryController.js";
 
-const routes = Router()
+const routes = Router();
 
-routes.get('/movies-categories', getMoviesAndCategories)
-routes.get('/movies', getMovies)
-routes.post('/movie', createMovie)
-routes.delete('/movie/:id', deleteMovie)
-routes.put('/movie/:id', updateMovie)
+routes.get("/movies-categories", getMoviesAndCategories);
+routes.get("/movies", getMovies);
+routes.post("/movie", createMovie);
+routes.delete("/movie/:id", deleteMovie);
+routes.put("/movie/:id", updateMovie);
 
-routes.get('/categories', getCategories)
-routes.post('/category', createCategory)
-routes.delete('/category/:id', deleteCategory)
-routes.put('/category/:id', updateCategory)
+routes.get("/categories", getCategories);
+routes.post("/category", createCategory);
+routes.delete("/category/:id", deleteCategory);
+routes.put("/category/:id", updateCategory);
 
-export default routes
+export default routes;
